ba25efcf5d4406c3b78dc84238417bcb6da54995
tangbo <837334754@qq.com>
2025-05-03 08:43:55 +0800

    1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -25,7 +25,7 @@ onMounted(() => {
   authStore.fetchServerInfo().then(() => {
     authStore.isAuthenticated = true
   })
-  console.log('Home')
+  console.log('Home1')
   if (!authStore.isAuthenticated) {
     // authStore.fetchUser()
   }
